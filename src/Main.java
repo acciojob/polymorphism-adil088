9569b4c783a76cc7734a32943d89247c0038f7a3
Mohd Adil <90774182+adil088@users.noreply.github.com>
2024-12-12 10:53:34 +0530

    Create Main.java

---
+++ src/Main.java
@@ -0,0 +1,27 @@
+public class Main {
+
+    static class Product {
+
+        public int product(int x, int y) {
+            return x * y;
+        }
+
+        public int product(int x, int y, int z) {
+            return z * x * y;
+        }
+
+        public double product(double x, double y) {
+            return x * y;
+        }
+
+    }
+
+    public static void main(String[] args) {
+        Product p = new Product();
+
+        System.out.println("p1: " + p.product(5, 10));
+        System.out.println("p2: " + p.product(5, 10));
+        System.out.println("p3: " + p.product(5.66d, 10.33d));
+
+    }
+}
